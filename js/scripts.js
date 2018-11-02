ec2659c379940abeecc02ae1d7272bcbf536ba42
HirokiNishida <hiroki.nishida@gmail.com>
2018-11-02 12:16:37 +0900

    ホームページ刷新

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+    /***************** Smooth Scroll ******************/
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+                return false;
+            }
+        }
+    });
+
+    /***************** Responsive Nav ******************/
+
+    $('.nav-toggle').click(function() {
+        $(this).toggleClass('active');
+        $('.navicon').toggleClass('fixed');
+        $('.primary-nav-wrapper').toggleClass('open');
+        event.preventDefault();
+    });
+    $('.primary-nav-wrapper li a').click(function() {
+        $('.nav-toggle').toggleClass('active');
+        $('.navicon').toggleClass('fixed');
+        $('.primary-nav-wrapper').toggleClass('open');
+    });
+
+    /***************** Waypoints ******************/
+
+    $('.wp1').waypoint(function() {
+        $('.wp1').addClass('animated fadeInUp');
+    }, {
+        offset: '80%'
+    });
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeInUp');
+    }, {
+        offset: '95%'
+    });
+    $('.wp3').waypoint(function() {
+        $('.wp3').addClass('animated fadeInUp');
+    }, {
+        offset: '95%'
+    });
+    $('.wp4').waypoint(function() {
+        $('.wp4').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function() {
+        $('.wp5').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function() {
+        $('.wp6').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp7').waypoint(function() {
+        $('.wp7').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+    $('.wp8').waypoint(function() {
+        $('.wp8').addClass('animated fadeIn');
+    }, {
+        offset: '75%'
+    });
+
+
+});
